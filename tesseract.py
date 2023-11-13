c6ef93d41801fa31fc8736f822060b4724a505bd
chteca <yulyakozeka@gmail.com>
2023-11-13 14:24:39 +0300

    change cessing

--- tesseract.py
+++ tesseract.py
@@ -18,7 +18,12 @@ def get_catalog_numbers(file_path, product_data=pd.read_csv('./data/prices.csv')
     
     image = Image.open(file_path)
     text = pytesseract.image_to_string(image)
-    text = text.replace(' ', '').replace('O', '0').replace('-', '')
+    text = text.replace(' ', '').replace('O', '0').replace('-', '').replace('!', '1')
     supposed_cat_num = [text[i:i+9] for i in range(len(text)-9)] + [text[i:i+10] for i in range(len(text)-10)]
     catalog_numbers = [c for c in supposed_cat_num if c in list(product_data.catalog_number.values)]
+    text = text.replace('I', '1')
+    changed_text_cn = [text[i:i+9] for i in range(len(text)-9)] + [text[i:i+10] for i in range(len(text)-10)]
+    changed_cn = [c for c in changed_text_cn if c in list(product_data.catalog_number.values) and c not in catalog_numbers]
+    catalog_numbers.extend(changed_cn)
+
     return catalog_numbers
